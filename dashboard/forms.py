e4387739dda93bf1614534744ace6c4af60aee6c
eduardolongencorrea <eduardolongencorrea@gmail.com>
2024-11-04 20:29:05 -0300

    fix register

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -3,16 +3,18 @@ from django import forms
 from django.contrib.auth.models import User
 
 class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput, label="Password")
     password_confirm = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'password_confirm']
 
-    def clean_password_confirm(self):
-        password = self.cleaned_data.get("password")
-        password_confirm = self.cleaned_data.get("password_confirm")
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+
         if password and password_confirm and password != password_confirm:
-            raise forms.ValidationError("Passwords do not match")
-        return password_confirm
+            self.add_error("password_confirm", "Passwords do not match.")
+        return cleaned_data
